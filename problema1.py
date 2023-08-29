62d7bf17e3deca84ef6d866dfe3ac46b62cb8322
JuanSV22 <127951508+JuanSV22@users.noreply.github.com>
2023-08-29 09:22:18 -0400

    Update problema1.py

--- problema1.py
+++ problema1.py
@@ -1,16 +1,4 @@
-def retRight(IN):
-    try:
-        return int(IN)
-    except:
-        return IN
+t1 = tuple(input())
+t2 = tuple(input())
 
-def removeStrangeComma(tup):
-    if len(tup) == 1:
-        print('('+str(tup[0])+')')
-    else:
-        print(tup)
-
-t1 = tuple([retRight(x) for x in input().split(' ') if x != ''])
-t2 = tuple([retRight(x) for x in input().split(' ') if x != ''])
-
-removeStrangeComma(t2+t1+t2)
+print(t2+t1+t2)
