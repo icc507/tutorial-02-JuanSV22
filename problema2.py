a8f4372644af3d9e3f9b82963a5ff2aaabe13346
JuanSV22 <127951508+JuanSV22@users.noreply.github.com>
2023-08-29 09:29:50 -0400

    Update problema2.py

--- problema2.py
+++ problema2.py
@@ -1,1 +1,7 @@
-print(tuple(reversed(input().split(' '))))
+def removeStrangeComma(tup):
+    if len(tup) == 1:
+        print('('+str(tup[0])+')')
+    else:
+        print(tup)
+
+removeStrangeComma(tuple(reversed(input().split(' '))))
