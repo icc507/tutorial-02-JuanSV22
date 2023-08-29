cb84cda086992fcb762f1ea2896464160eddf6c2
JuanSV22 <127951508+JuanSV22@users.noreply.github.com>
2023-08-29 09:05:01 -0400

    Update problema3.py

--- problema3.py
+++ problema3.py
@@ -1,13 +1,58 @@
-#Problema 2  / 7 ptos x4 pruebas / 28 puntos
-#Ingreso de valores en árbol TRI-nario
-#---------------------------------------------------------------------------------
-#Confeccione un programa que lea varios números y cree un árbol trinario con listas
-# esto es igual que el binario, pero los elementos que son iguales van en una lista
-# centro, de la forma [numero, [subarbol IZQ], [mismo NUM], [subarbol DER] ]
-#---------------------------------------------------------------------------------
-#Ejemplo de entrada:
-#         20 30 90 90 8 5 90
-#La salida debe ser
-#         [20, [8, [5, [], [], []], [], []], [], [30, [], [], [90, [], [90, [], [90, [], [], []], []], []]]]
-t = input()
-print(t)
+class NodoTrinario:
+    def __init__(self, valor):
+        self.valor = valor
+        self.izquierda = None
+        self.iguales = []
+        self.derecha = None
+
+
+def insertar_nodo(raiz, valor):
+    if raiz is None:
+        return NodoTrinario(valor)
+
+    if valor == raiz.valor:
+        raiz.iguales.append(valor)
+    elif valor < raiz.valor:
+        raiz.izquierda = insertar_nodo(raiz.izquierda, valor)
+    else:
+        raiz.derecha = insertar_nodo(raiz.derecha, valor)
+
+    return raiz
+
+
+def construir_arbol_trinario(valores):
+    raiz = None
+    for valor in valores:
+        if raiz is None:
+            raiz = NodoTrinario(valor)
+        else:
+            insertar_nodo(raiz, valor)
+    return raiz
+
+
+def generar_salida_arbol_trinario(raiz):
+    if raiz is None:
+        return "[]"
+
+    salida = "["
+    salida += str(raiz.valor)
+
+    if raiz.iguales:
+        salida += ", " + str(raiz.iguales)
+    else:
+        salida += ", [],"
+
+    salida += " " + generar_salida_arbol_trinario(raiz.izquierda)
+    salida += ", " + generar_salida_arbol_trinario(raiz.derecha)
+    salida += "]"
+
+    return salida
+
+
+valores_entrada = input().split()
+valores_entrada = list(map(int, valores_entrada))
+
+arbol_trinario = construir_arbol_trinario(valores_entrada)
+
+salida = generar_salida_arbol_trinario(arbol_trinario)
+print(salida)
