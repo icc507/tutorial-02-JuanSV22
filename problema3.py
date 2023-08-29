d19e8ffa0a89c620f803997ff90eb000142db2f9
JuanSV22 <127951508+JuanSV22@users.noreply.github.com>
2023-08-29 10:29:05 -0400

    Update problema3.py

--- problema3.py
+++ problema3.py
@@ -1,58 +1,19 @@
-class NodoTrinario:
-    def __init__(self, valor):
-        self.valor = valor
-        self.izquierda = None
-        self.iguales = []
-        self.derecha = None
+def arbolTrinario(numero):
+	return [numero, [], [], []]
+
+def insertaEnArbolTrinario(arbol,numero):
+	if arbol==[]:
+		arbol+=arbolTrinario(numero)
+	elif numero < arbol[0]:
+		insertaEnArbolTrinario(arbol[1],numero)
+	elif numero == arbol[0]:
+		insertaEnArbolTrinario(arbol[2],numero)
+	else:
+		insertaEnArbolTrinario(arbol[3],numero)
 
 
-def insertar_nodo(raiz, valor):
-    if raiz is None:
-        return NodoTrinario(valor)
+w = []
 
-    if valor == raiz.valor:
-        raiz.iguales.append(valor)
-    elif valor < raiz.valor:
-        raiz.izquierda = insertar_nodo(raiz.izquierda, valor)
-    else:
-        raiz.derecha = insertar_nodo(raiz.derecha, valor)
-
-    return raiz
-
-
-def construir_arbol_trinario(valores):
-    raiz = None
-    for valor in valores:
-        if raiz is None:
-            raiz = NodoTrinario(valor)
-        else:
-            insertar_nodo(raiz, valor)
-    return raiz
-
-
-def generar_salida_arbol_trinario(raiz):
-    if raiz is None:
-        return "[]"
-
-    salida = "["
-    salida += str(raiz.valor)
-
-    if raiz.iguales:
-        salida += ", " + str(raiz.iguales)
-    else:
-        salida += ", [],"
-
-    salida += " " + generar_salida_arbol_trinario(raiz.izquierda)
-    salida += ", " + generar_salida_arbol_trinario(raiz.derecha)
-    salida += "]"
-
-    return salida
-
-
-valores_entrada = input().split()
-valores_entrada = list(map(int, valores_entrada))
-
-arbol_trinario = construir_arbol_trinario(valores_entrada)
-
-salida = generar_salida_arbol_trinario(arbol_trinario)
-print(salida)
+for x in (20, 30, 90, 90, 8, 5, 90):
+    insertaEnArbolTrinario(w,x)
+print(w)
