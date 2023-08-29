e10f54a8563c5eda915d677743a7b6741f4192f7
JuanSV22 <127951508+JuanSV22@users.noreply.github.com>
2023-08-29 09:32:08 -0400

    Update problema2.py

--- problema2.py
+++ problema2.py
@@ -1,1 +1,7 @@
-print(tuple(reversed(input().split(' '))))
+def retRight(IN):
+    try:
+        return int(IN)
+    except:
+        return IN
+      
+print(tuple(retRight(x) for x in reversed(input().split(' '))))
