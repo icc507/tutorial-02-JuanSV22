85b6e3df39d5aca9a6fe5f6833509493c0a71b10
JuanSV22 <127951508+JuanSV22@users.noreply.github.com>
2023-08-29 09:27:58 -0400

    Update problema1.py

--- problema1.py
+++ problema1.py
@@ -1,4 +1,10 @@
-t1 = tuple(input().split(' '))
-t2 = tuple(input().split(' '))
+def retRight(IN):
+    try:
+        return int(IN)
+    except:
+        return IN
+
+t1 = tuple([retRight(x) for x in input().split(' ')])
+t2 = tuple([retRight(x) for x in input().split(' ')])
 
 print(t2+t1+t2)
